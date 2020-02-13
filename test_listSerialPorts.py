3574c422b16bdf9b7cf4b1bec8b5c6bcf58a9ff7
yayar0103 <nnico12341tw@gmail.com>
2020-02-13 18:13:53 +0800

    update test_listSerialPorts.py

--- test_listSerialPorts.py
+++ test_listSerialPorts.py
@@ -8,9 +8,6 @@ Created on Thu Feb 13 15:30:40 2020
 
 import lora_util
 
-device = lora_util.LoRa()
+Lora = lora_util.LoRa()
 print("List All Serial Ports")
-ser = device.serial_allPorts()
-print(ser)
-print("List All Serial Ports")
-device.new_serial_allPorts()
+ports = Lora.listPorts()
