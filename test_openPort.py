c5fd2281696d8539285a84b12b96c0dba27ea35b
StevenChu-ccp <stnc5120@gmail.com>
2020-02-14 14:52:36 +0800

    new test of function openPort()

---
+++ test_openPort.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 14 14:37:56 2020
+
+@author: Steven
+"""
+
+
+import lora_util
+
+lora = lora_util.LoRa()
+
+#correct port
+try:
+    port = ['dev/ttyACM0']
+    print('Port status: ', lora.openPort(port))
+except:
+    print('Failed to open port')
+
+#unexist port
+try:
+    port = ['dev/ttyACM1']
+    print('Port status: ', lora.openPort(port))
+except:
+    print('Failed to open port')
