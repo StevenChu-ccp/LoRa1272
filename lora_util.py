23c9da9cc8743c195b8e11f98527ee4937468ec9
StevenChu-ccp <stnc5120@gmail.com>
2020-02-17 19:02:08 +0800

    update 2/17/20 not tested yet

--- lora_util.py
+++ lora_util.py
@@ -75,7 +75,7 @@ class LoRa:
             self.serialPort = self.openPort(port.device)
             
             #change to __testiflora()
-            data=self.GetLoRaID()
+            data=self.getLoRaID()
             print(data)
             if(len(data) > 1):
                 return port.device
@@ -90,14 +90,12 @@ class LoRa:
     Get the details of the device
     return: Hex data
     """
-    def GetLoRaID(self):
+    def getLoRaID(self):
         packet_hdr = [0x80, 0x00, 0x00, 0]
         packet_hdr[3] = self.__calCRC(packet_hdr)
         if self.debug == True:
             print(packet_hdr)
-        
         self.serialWrite(packet_hdr)
-        
         time.sleep(0.01)
         bytesToRead = self.serialPort.inWaiting()
         data = self.serialPort.read(bytesToRead)
@@ -125,3 +123,71 @@ class LoRa:
             self.serialPort.write(serial.to_bytes(data))
         except serial.SerialExecption as ex:
             raise OSError(ex)
+            
+    
+    """
+    Communicate with LoRa
+    """
+    def sendByteToChip(self, data):
+        try:
+            if self.debug == True:
+                print(data)
+            self.serialPort.write(serial.to_bytes(data))
+            time.sleep(0.04)
+            bytesToRead = self.serialPort.inWaiting()
+            cnt = 0
+            while True:
+                readData = self.serialPort.read(bytesToRead)
+                cnt += 1
+                
+                #if sucess or time out, break
+                if len(data) > 0 or cnt > self.waitCount:
+                    break
+            if self.debug == True:
+                print(readData.hex())
+        #except (OSError, serial.SerialException):
+        except serial.SerialExecption as ex:
+            raise OSError(ex)
+        return readData
+    
+    
+    """
+    Close serial port
+    """
+    def serialClose(self):
+        try:
+            self.serialPort.close()
+        except serial.SerialException:
+            print("port already open")
+            
+    
+    ##################################################
+    #Application
+    ##################################################
+    
+    def initLoRa(self):
+        initCmd = [0xc1, 0x01, 0x00, 0]
+        initCmd[3] = self.__calCRC(initCmd)
+        data = self.sendByteToChip(initCmd)
+        return data
+    
+    
+    def readSetup(self):
+        package_hdr = [0xc1,0x02,0x00,0]
+        package_hdr[3] = self.Fun_CRC(package_hdr)
+        data = self.FunLora_ChipSendByte(package_hdr)
+        return data
+    
+    
+    def RX(self):
+       array1 = [0xC1,3,5,3,1,0x65,0x6C,0x0f,0]
+       array1[8] = self.Fun_CRC(array1)
+       data=self.FunLora_ChipSendByte(array1)
+       return data
+
+    # 設定寫入和頻段
+    def TX(self):
+       array1 = [0xC1,3,5,2,1,0x65,0x6C,0x0f,0]
+       array1[8] = self.Fun_CRC(array1)
+       data = self.FunLora_ChipSendByte(array1)
+       return data
