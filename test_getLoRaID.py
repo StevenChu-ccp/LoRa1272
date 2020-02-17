d0986d3ebc95b6688a2993a04420b6a94d211760
StevenChu-ccp <stnc5120@gmail.com>
2020-02-17 19:02:58 +0800

    New test case 2/17/20

---
+++ test_getLoRaID.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 17 18:48:37 2020
+
+@author: Steven
+"""
+
+
+import lora_util
+
+lora = lora_util.LoRa()
+ports = lora.listPorts()
+lora_path = lora.findLoRa(ports)
+lora.openPort(lora_path)
+
+print(lora.getLoRaID())
+
+lora.serialClose()
